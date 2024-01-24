6ab3b01f2fc3fef18022c9f938dc488f7a867876
Chris Thapa <141107824+LeanQChris@users.noreply.github.com>
2024-01-24 15:32:57 +0545

    jokes api no-nocache

--- src/app/jokes/data/api.ts
+++ src/app/jokes/data/api.ts
@@ -1,6 +1,6 @@
 import { Joke } from "./entity";
 
 export const fetchRandomJokes = async ():Promise<Joke> => {
-    const response = await fetch("https://official-joke-api.appspot.com/jokes/random",{cache:"reload"});
+    const response = await fetch("https://official-joke-api.appspot.com/jokes/random",{cache:"no-store"});
     return response.json();
 };
